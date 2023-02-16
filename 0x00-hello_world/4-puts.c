e659c4ff5efdf98cc0df2d121c8fb459c124b7c2
lilsparky222 <lily.dzamesi@yahoo.com>
2023-02-16 12:15:12 -0800

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void)
-{
-	/** 
-	 * main- function for main
-	 *
-	 * retun 0 always
-	 */
 
+	/**
+	 * main- main point
+	 *
+	 * Return: returns 
+	 */
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
