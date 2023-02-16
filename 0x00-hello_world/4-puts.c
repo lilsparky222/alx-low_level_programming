c55cc89f7e0238a026f0e2d7645c8858f4608a98
lilsparky222 <lily.dzamesi@yahoo.com>
2023-02-16 12:29:18 -0800

    hello ptintf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-int main(void)
 
 	/**
 	 * main- main point
 	 *
 	 * Return: returns 
 	 */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+	/**
+	 * main - main function
+	 *
+	 * return : always 0
+	 */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of ar\nt");
+	return (0);
+}
