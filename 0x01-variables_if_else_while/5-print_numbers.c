99a6a28655755a6ef17c9114a5c245d843e1dc49
lilsparky222 <lily.dzamesi@yahoo.com>
2023-02-17 12:13:35 -0800

    alphabet excep q and e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** main - main entry block
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num =0; num < 10;num++)
+	{
+		printf("%i", num);
+	}
+	putchar('\n');
+	return (0);
+}
+
