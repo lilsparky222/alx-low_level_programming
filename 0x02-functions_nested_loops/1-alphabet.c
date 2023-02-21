69bf75d3a8a1882956291740288a8b876d4fe0a1
lilsparky222 <lily.dzamesi@yahoo.com>
2023-02-21 11:57:59 -0800

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - inlowercase
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
