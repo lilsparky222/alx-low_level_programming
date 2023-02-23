286435820ad5fb8b4d02c5dca320cc258d893ef8
lilsparky222 <lily.dzamesi@yahoo.com>
2023-02-23 14:49:58 -0800

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: Prints lines
+ *@n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+int lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lines = 1; lines <= n; lines++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
