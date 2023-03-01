f8cb2148a6766ffd8d361bae44ac0c3a8272b5bf
lilsparky222 <lily.dzamesi@yahoo.com>
2023-03-01 12:30:52 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
