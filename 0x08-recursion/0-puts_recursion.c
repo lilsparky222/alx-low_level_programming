3c1d77be2563cbc00fb4d6a32c8fb46a4cbbd8ad
lilsparky222 <lily.dzamesi@yahoo.com>
2023-03-08 11:40:33 -0800

    recurssion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		putchar('\n');
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
