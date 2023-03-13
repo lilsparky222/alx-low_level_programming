432b6e2757e527d4aa475d23e69253724f599ce7
lilsparky222 <lily.dzamesi@yahoo.com>
2023-03-13 09:20:47 -0700

    libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - convert to uppercase
+ * @c: char character
+ *
+ * Return: 0 
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
+}
+
