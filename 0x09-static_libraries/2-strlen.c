ccc6c51da05d8764167faa0ed142beb03742f53c
lilsparky222 <lily.dzamesi@yahoo.com>
2023-03-13 10:44:44 -0700

    lib

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: char input
- * Return: length
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
-	int i = 1, sum = 0;
-	char pl = s[0];
+	int i;
 
-	while (pl != '\0')
+	i = 0;
+	while (*s != '\0')
 	{
-		sum++;
-		pl = s[i++];
+		i++;
+		s++;
 	}
-	return (sum);
+	return (i);
 }
